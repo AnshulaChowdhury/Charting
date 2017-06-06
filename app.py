4e84fe3a2c12dabc417744cd5182085b9c32dd29
Angad Dubey <angad_dubey@Hotmail.com>
2017-06-06 08:10:13 -0400

    refactoring

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask import Markup
-from flask import Flask
+#from flask import Flask - Already imported on above
 from flask_mysqldb import MySQL
-from flask.ext.mysql import MySQL
+#from flask.ext.mysql import MySQL - dont need this - already imported above
 from flask import render_template
 import Algorithmia
-import MySQLdb
+#import MySQLdb - NO such module
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'C:\Program Files\MySQL\MySQL Server 5.7\bin'
+#app.config['MYSQL_HOST'] = 'C:\Program Files\MySQL\MySQL Server 5.7\bin'
+app.config['MYSQL_HOST'] = 'localhost' # host requires the network address of mysql
 app.config['MYSQL_USER'] = 'root@localhost'
 app.config['MYSQL_PASSWORD'] ='root'
-app.config['MYSQL_DB'] = 'EmpData.sql'
+#app.config['MYSQL_DB'] = 'EmpData.sql'
+app.config['MYSQL_DB'] = 'anshula_charting' # Needs a DB name. Use this command to create: create database anshula_charting;
 mysql = MySQL(app)
 
 @app.route("/")
